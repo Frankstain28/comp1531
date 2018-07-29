94e713b49e70e54342337e08b344ae979161a720
Frankstain28 <frankstainwang@gmail.com>
2018-07-29 15:52:39 +1000

    Added a minimal application

--- lab01-Github-lab-release/strings.py
+++ lab01-Github-lab-release/strings.py
@@ -1,2 +1,8 @@
 strings = ['This', 'list', 'is', 'now', 'all', 'together']
 sentence = ''
+
+for i in range(len(strings)):
+    sentence += strings[i] + ' '
+
+print(sentence.strip())
+print(' '.join(strings))
